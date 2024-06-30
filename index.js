6bd977d94b3a7bc02a3975114a883a9a2bd47ed1
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-06-30 23:19:46 +0300

    string lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+// Code your solution in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
